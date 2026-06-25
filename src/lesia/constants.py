1b1941f6c1be090d4dddd9635cc3716726f32996
Yehor Korotenko <dobbikov@gmail.com>
2026-06-25 14:13:45 +0200

    feat: renamed translation_cache to cache in the .lesia directory

--- src/lesia/constants.py
+++ src/lesia/constants.py
@@ -2,7 +2,7 @@
 CONFIG_FILENAME = "config.json"
 INTER_FILE_TRANSLATION_DELAY_SECONDS = 5 
 
-CACHE_DIR_NAME = "translate_cache"
+CACHE_DIR_NAME = "cache"
 CORRESPONDENCE_CACHE_FILENAME = "correspondence_cache.csv"
 PATH_MAP_FILENAME = "path_checksums.csv"
 
